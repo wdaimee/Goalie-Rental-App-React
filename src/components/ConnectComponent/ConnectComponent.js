0adb39bbed3615f3139eeeb0709b8be303e13976
Waqas D <wdaimee@gmail.com>
2020-05-03 12:29:07 -0400

    added connect component code

--- src/components/ConnectComponent/ConnectComponent.js
+++ src/components/ConnectComponent/ConnectComponent.js
@@ -1,14 +1,18 @@
 import React from 'react';
+import './ConnectComponent.css';
 
 export default function ConnectComponent() {
     return (
         <div className="container-fluid padding">
             <div className="row text-center padding">
-                <div class="col-12">
-                    <h2>Connect</h2>
+                <div className="col-12">
+                    <h2>Come Check Us Out Below</h2>
                 </div>
                 <div className="col-12 social padding">
-                    <a href="#"><i class="fab fa-face</a>
+                    <a href="#"><i class="fab fa-facebook"></i></a>
+                    <a href="#"><i class="fab fa-twitter"></i></a>
+                    <a href="#"><i class="fab fa-instagram"></i></a>
+                    <a href="#"><i class="fab fa-youtube"></i></a>
                 </div>
             </div>
         </div>
--- src/pages/LandingPage/LandingPage.js
+++ src/pages/LandingPage/LandingPage.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
 import CarouselComponent from '../../components/CarouselComponent/CarouselComponent';
+import ConnectComponent from '../../components/ConnectComponent/ConnectComponent';
 
 class LandingPage extends Component {
     render() {
         return(
-            <CarouselComponent />
+            <>
+                <CarouselComponent />
+                <ConnectComponent />
+            </>    
         );
     };
 }
