1b812cb522c03902de40c84853752597a832df51
Waqas D <wdaimee@gmail.com>
2020-05-07 15:26:08 -0400

    updated navbar links to get client side routing working

--- src/components/NavbarComponent/NavbarComponent.js
+++ src/components/NavbarComponent/NavbarComponent.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
+import { Link } from 'react-router-dom';
 
 export default function NavbarComponent(props) {
    
@@ -8,22 +9,22 @@ export default function NavbarComponent(props) {
     let nav_goalie1;
     let nav_goalie2;
     if (props.user) {
-        nav_goalie1 = props.user.goalie ? <Nav.Link href="/games">Find Games</Nav.Link> : null;
-        nav_goalie2 = props.user.goalie ? <Nav.Link href="/mygames">My Games</Nav.Link> : null;
+        nav_goalie1 = props.user.goalie ? <Nav.Link as={Link} to="/games">Find Games</Nav.Link> : null;
+        nav_goalie2 = props.user.goalie ? <Nav.Link as={Link} to="/mygames">My Games</Nav.Link> : null;
     }
 
     let nav = props.user ?
     <Navbar collapseOnSelect expand="lg" variant="dark" style={{backgroundColor: "black", color: "white", fontSize: "1.5rem"}}>
-        <Navbar.Brand style={{fontSize: "1.5rem"}} href="/">gÜber - The Goalie Rental Company</Navbar.Brand>
+        <Navbar.Brand style={{fontSize: "1.5rem"}} as={Link} to="/">gÜber - The Goalie Rental Company</Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
             <Nav className="ml-auto">
                 {nav_goalie1}
                 {nav_goalie2}
-                <Nav.Link href="/requests">My Requests</Nav.Link>
-                <Nav.Link href="/create">Make Request</Nav.Link>
-                <Nav.Link href="/profile">Profile</Nav.Link>
-                <Nav.Link href="/" onClick={props.handleLogout}>Log Out</Nav.Link>
+                <Nav.Link as={Link} to="/requests">My Requests</Nav.Link>
+                <Nav.Link as={Link} to="/create">Make Request</Nav.Link>
+                <Nav.Link as={Link} to="/profile">Profile</Nav.Link>
+                <Nav.Link as={Link} to="/" onClick={props.handleLogout}>Log Out</Nav.Link>
             </Nav>
         </Navbar.Collapse>
     </Navbar>
@@ -33,8 +34,8 @@ export default function NavbarComponent(props) {
            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
            <Navbar.Collapse id="responsive-navbar-nav">
                <Nav className="ml-auto">
-                   <Nav.Link href="/signup">Signup</Nav.Link>
-                   <Nav.Link href="/login">Log In</Nav.Link>
+                   <Nav.Link as={Link} to="/signup">Signup</Nav.Link>
+                   <Nav.Link as={Link} to="/login">Log In</Nav.Link>
                </Nav>
            </Navbar.Collapse>
     </Navbar>
